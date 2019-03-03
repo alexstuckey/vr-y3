ac8a254ceb2f8253786133d2d2f3108bd3a34883
Alex Stuckey <alex@stuckey.org.uk>
2019-03-03 20:17:48 +0000

    Scaffolding of solution to problem 1

---
+++ problem_1.py
@@ -0,0 +1,31 @@
+import file
+
+
+# Methods
+
+# (i) convert Euler angle readings (radians) to quaternions (2 marks)
+def i_radians_to_quaternions():
+    return
+
+
+# (ii) calculate Euler angles from a quaternion (2 marks)
+def ii_quaternion_to_radians():
+    return
+
+
+# (iii) convert a quaternion to its conjugate (inverse rotation) (2 marks)
+def iii_quaternion_inverse_rotation():
+    return
+
+
+# (iv) calculate the quaternion product of quaternion a and b (2 marks)
+def iv_quaternion_product(a, b):
+    return
+
+
+# Read and import the provided (.csv) dataset (5 marks)
+
+
+# Convert rotational rate to radians/sec and normalize the magnitude
+# of both the accelerometer and magnetometer values
+# taking special care of NaN divisions (5 marks)
