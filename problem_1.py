5d6abf0aafe4eb88cd7f4852ce6d033a9898c398
Alex Stuckey <alex@stuckey.org.uk>
2019-03-03 21:53:46 +0000

    Normalising accelerometer and magnetometer readings

--- problem_1.py
+++ problem_1.py
@@ -31,16 +31,33 @@ with open('imu-data.csv') as csvfile:
     csv_reader = csv.DictReader(csvfile)
     csv_reader.fieldnames = [field.strip() for field in csv_reader.fieldnames]
 
-    for line in csv_reader:
-        newDict = dict(line)
+    for row in csv_reader:
+        newDict = {}
+        newDict['time'] = float(row['time'])
 
         # Convert rotational rate to radians/sec
-        newDict['gyroscope.X'] = math.radians(float(newDict['gyroscope.X']))
-        newDict['gyroscope.Y'] = math.radians(float(newDict['gyroscope.Y']))
-        newDict['gyroscope.Z'] = math.radians(float(newDict['gyroscope.Z']))
+        newDict['gyroscope.X'] = math.radians(float(row['gyroscope.X']))
+        newDict['gyroscope.Y'] = math.radians(float(row['gyroscope.Y']))
+        newDict['gyroscope.Z'] = math.radians(float(row['gyroscope.Z']))
 
         # Normalize magnitude of both the accelerometer and magnetometer values
         # taking special care of NaN divisions (5 marks)
+        acc_sum = \
+            (float(row['accelerometer.X']) * float(row['accelerometer.X'])) \
+            + (float(row['accelerometer.Y']) * float(row['accelerometer.Y'])) \
+            + (float(row['accelerometer.Z']) * float(row['accelerometer.Z']))
+        acc_norm = math.sqrt(acc_sum)
+        newDict['accelerometer.X'] = float(row['accelerometer.X']) / acc_norm
+        newDict['accelerometer.Y'] = float(row['accelerometer.Y']) / acc_norm
+        newDict['accelerometer.Z'] = float(row['accelerometer.Z']) / acc_norm
 
+        mag_sum = \
+            (float(row['magnetometer.X']) * float(row['magnetometer.X'])) \
+            + (float(row['magnetometer.Y']) * float(row['magnetometer.Y'])) \
+            + (float(row['magnetometer.Z']) * float(row['magnetometer.Z']))
+        mag_norm = math.sqrt(mag_sum)
+        newDict['magnetometer.X'] = float(row['magnetometer.X']) / mag_norm
+        newDict['magnetometer.Y'] = float(row['magnetometer.Y']) / mag_norm
+        newDict['magnetometer.Z'] = float(row['magnetometer.Z']) / mag_norm
 
         print(newDict)
