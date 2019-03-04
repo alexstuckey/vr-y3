7b2a594be02e18b9cc3d39be76e012d3065981e0
Alex Stuckey <alex@stuckey.org.uk>
2019-03-04 16:33:09 +0000

    Functions to and from Euler angles

--- problem_1.py
+++ problem_1.py
@@ -2,16 +2,61 @@ import csv
 import math
 
 
+# My definition of a quaternion as a 4-tuple,
+# of 4 real numbers
+# (qw, qx i, qy j, qz k)
+
 # Methods
 
 # (i) convert Euler angle readings (radians) to quaternions (2 marks)
-def i_radians_to_quaternions():
-    return
+def i_radians_to_quaternions(x, y, z):
+    # Implementation assistance from
+    # https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+
+    # Euler angle
+    #  x-axis = roll
+    cr = math.cos(x * 0.5)
+    sr = math.sin(x * 0.5)
+    #  y-axis = pitch
+    cp = math.cos(y * 0.5)
+    sp = math.sin(y * 0.5)
+    #  z-axis = yaw
+    cy = math.cos(z * 0.5)
+    sy = math.sin(z * 0.5)
+
+    q = (cy * cp * cr + sy * sp * sr,
+         cy * cp * sr - sy * sp * cr,
+         sy * cp * sr + cy * sp * cr,
+         sy * cp * cr - cy * sp * sr)
+
+    return q
 
 
 # (ii) calculate Euler angles from a quaternion (2 marks)
-def ii_quaternion_to_radians():
-    return
+def ii_quaternion_to_radians(q):
+    # Implementation assistance from
+    # https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+
+    # roll (x-axis rotation)
+    sinr_cosp = +2.0 * (q[0] * q[1] + q[2] * q[3])
+    cosr_cosp = +1.0 - 2.0 * (q[1] * q[1] + q[2] * q[2])
+    roll = math.atan2(sinr_cosp, cosr_cosp)
+
+    # pitch (y-axis rotation)
+    sinp = +2.0 * (q[0] * q[2] - q[3] * q[1])
+    if (math.fabs(sinp) >= 1):
+        # use 90 degrees if out of range
+        pitch = math.copysign(math.pi / 2, sinp)
+    else:
+        pitch = math.asin(sinp)
+
+    # yaw (z-axis rotation)
+    siny_cosp = +2.0 * (q[0] * q[3] + q[1] * q[2])
+    cosy_cosp = +1.0 - 2.0 * (q[2] * q[2] + q[3] * q[3])
+    yaw = math.atan2(siny_cosp, cosy_cosp)
+
+    euler = (roll, pitch, yaw)
+    return euler
 
 
 # (iii) convert a quaternion to its conjugate (inverse rotation) (2 marks)
