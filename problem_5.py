72d60ee10513de10c694da701ef167c9ebbee8f3
Alex Stuckey <alex@stuckey.org.uk>
2019-03-14 19:04:21 +0000

    Update problem_5.py

--- problem_5.py
+++ problem_5.py
@@ -1,19 +1,20 @@
 import problem_1
 import problem_2
 import problem_3
+import problem_4
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 
 # Import data
-dataset = problem_1.load_dataset()
-filter_gyro = problem_2.dead_reckoning_gyroscope()
-filter_gyro_acc = problem_3.dead_reckoning_gyroscope_accelerometer(alpha=0.01)
-
+# dataset is added to through each problem 1, 2, 3, 4
+# problem_1.load_dataset(), problem_2.dead_reckoning_gyroscope(),
+#    problem_3.dead_reckoning_tilt(alpha=0.001)
+dataset = problem_4.dead_reckoning_yaw(alpha=0.001)[1:]
 
 my_dpi = 100
 fig, ax = plt.subplots(figsize=(1280 / my_dpi, 720 / my_dpi),
                        dpi=my_dpi,
-                       nrows=3, ncols=3)
+                       nrows=2, ncols=3)
 
 # Render lines
 ax[0][0].set_xlabel('Time')
@@ -61,27 +62,20 @@ ax[0][2].plot([row['time'] for row in dataset],
 ax[1][0].set_xlabel('Time')
 ax[1][0].set_ylabel(r'Orientation (°$\,s^{-1}$)')
 ax[1][0].set_title('Gyroscope')
-ax[1][0].plot([row['time'] for row in filter_gyro],
-              [row['est_gyro_deg'] for row in filter_gyro])
+ax[1][0].plot([row['time'] for row in dataset],
+              [row['est_gyro_deg'] for row in dataset])
 
 
 ax[1][1].set_xlabel('Time')
 ax[1][1].set_ylabel(r'Orientation (°$\,s^{-1}$)')
-ax[1][1].set_title('Gyroscope + Accelerometer')
-ax[1][1].plot([row['time'] for row in filter_gyro_acc],
+ax[1][1].set_title('Gyroscope + Tilt Filter')
+ax[1][1].plot([row['time'] for row in dataset],
               [row['est_tilt_deg'] for row in dataset])
 
 ax[1][2].set_xlabel('Time')
 ax[1][2].set_ylabel(r'Orientation (°$\,s^{-1}$)')
-ax[1][2].set_title('Gyroscope + Accelerometer + Magnetometer')
+ax[1][2].set_title('Gyroscope + Tilt Fitler + Yaw Filter')
 
-plt.delaxes(ax[2][0])
-ax[2][0] = fig.add_subplot(3, 3, 7, projection='3d')
-# for frame in range(0, len(filter_gyro)):
-#     print(filter_gyro[frame]['time'],
-#           filter_gyro[frame]['est_gyro'])
-#     plt.plot(filter_gyro[frame]['time'],
-#              filter_gyro[frame]['est_gyro'])
 
 plt.tight_layout()
 plt.savefig('testplot.png')
