6386d21e6394d7aa3b7903d449ca0a436213b305
Alex Stuckey <alex@stuckey.org.uk>
2019-03-14 19:47:58 +0000

    Implementing first version of yaw filter

--- problem_4.py
+++ problem_4.py
@@ -12,46 +12,43 @@ def dead_reckoning_yaw(alpha_yaw=0.001, alpha_tilt=0.001):
 
     dataset = problem_3.dead_reckoning_tilt(alpha_tilt=alpha_tilt)
 
+    # Reference from first magnetometer sample
+    m_ref = (0.0, *dataset[1]['m'])
+    q_ref = (1.0, 0.0, 0.0, 0.0)
+
     for k in range(1, len(dataset)):
         delta_t = dataset[k]['time'] - dataset[k - 1]['time']
 
         qs_k = dataset[k]['est_tilt_q']
 
-        # Transform acceleration measurements into the global frame (2 marks)
-        a = dataset[k]['a']
-        a = problem_1.iv_quaternion_product(
+        m_prime = (0.0, *dataset[k]['m'])
+        m_prime = problem_1.iv_quaternion_product(
             problem_1.iv_quaternion_product(
                 problem_1.iii_quaternion_inverse_rotation(qs_k),
-                (0, *a)
+                (0, *m_prime)
+            ),
+            qs_k
+        )
+        m_ref = problem_1.iv_quaternion_product(
+            problem_1.iv_quaternion_product(
+                problem_1.iii_quaternion_inverse_rotation(qs_k),
+                (0, *m_ref)
             ),
             qs_k
         )
 
-        # Trim off first element to make as vector
-        a = a[1:]
+        # Project into XY plane
+        m_ref = (m_ref[0], m_ref[1], 0)
+        m_prime = (m_prime[0], m_prime[1], 0)
 
-        # project into XY plane
-        a = (a[0], a[1], 0)
+        # Calculate thetas, to be used to find difference
+        theta = math.atan2(m_prime[0], m_prime[2])
+        theta_r = math.atan2(m_ref[0], m_ref[2])
 
-        # Calculate the tilt axis (2 marks)
-        t = (a[1], -a[0], 0)
-
-        # Normalise a
-        a_norm = a / np.linalg.norm(a)
-
-        # Find the angle φ between the up vector and the vector obtained from
-        #   the accelerometer (2 marks)
-        phi = np.arccos(np.clip(
-            np.dot(a_norm, (0, 0, 1)),
-            -1.0,
-            1.0
-        ))
-
-        # Use the complementary filter to fuse the two signals (7 marks).
         qs_k = problem_1.iv_quaternion_product(
             problem_2.make_q(
-                t,
-                ((-alpha) * phi)
+                (0, 1, 0),
+                ((-alpha_yaw) * (theta - theta_r))
             ),
             qs_k
         )
